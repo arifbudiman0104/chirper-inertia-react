145e3d59d8043cc8f3d8bfc6aa657452e2fb4059
Arif Budiman Arrosyid <arifbudiman01041996@hotmail.com>
2023-03-30 07:44:05 +0700

    add dark mode support

--- resources/js/Components/Chirp.jsx
+++ resources/js/Components/Chirp.jsx
@@ -38,13 +38,13 @@ export default function Chirp({ chirp }) {
                         <Dropdown>
                             <Dropdown.Trigger>
                                 <button>
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400 dark:text-gray-100" viewBox="0 0 20 20" fill="currentColor">
                                         <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                     </svg>
                                 </button>
                             </Dropdown.Trigger>
                             <Dropdown.Content>
-                                <button className="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 transition duration-150 ease-in-out" onClick={() => setEditing(true)}>
+                                <button className="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 dark:text-gray-100 hover:bg-gray-100 dark:hover:bg-gray-600 focus:bg-gray-100 dark:focus:bg-gray-700 transition duration-150 ease-in-out" onClick={() => setEditing(true)}>
                                     Edit
                                 </button>
                             </Dropdown.Content>
@@ -54,14 +54,14 @@ export default function Chirp({ chirp }) {
                 {/* <p className="mt-4 text-lg text-gray-900 dark:text-gray-300">{chirp.message}</p> */}
                 {editing
                     ? <form onSubmit={submit}>
-                        <textarea value={data.message} onChange={e => setData('message', e.target.value)} className="mt-4 w-full text-gray-900 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"></textarea>
+                        <textarea value={data.message} onChange={e => setData('message', e.target.value)} rows="3" className="mt-4 block w-full text-gray-900 dark:text-gray-300 placeholder:text-gray-900 dark:placeholder:text-gray-300 border-gray-300 dark:border-gray-900 bg-gray-100 dark:bg-gray-900 focus:border-indigo-300 dark:focus:border-indigo-500 focus:ring focus:ring-indigo-200 dark:focus:ring-indigo-400 focus:ring-opacity-50 rounded-md shadow-sm"></textarea>
                         <InputError message={errors.message} class="mt-2" />
                         <div className="space-x-2">
                             <PrimaryButton className="mt-4">Save</PrimaryButton>
-                            <button className="mt-4" onClick={() => { setEditing(false); reset(); clearErrors(); }}>Cancel</button>
+                            <button className="mt-4 text-gray-900 dark:text-gray-300" onClick={() => { setEditing(false); reset(); clearErrors(); }}>Cancel</button>
                         </div>
                     </form>
-                    : <p className="mt-4 text-lg text-gray-900">{chirp.message}</p>
+                    : <p className="mt-4 text-lg text-gray-900 dark:text-gray-300">{chirp.message}</p>
                 }
             </div>
         </div>
